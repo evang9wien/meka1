df9ed02c279a9c9a28e63da7de0e7f606c876af2
Andreas Eidmann <eidmanna@gmail.com>
2023-11-10 22:58:01 +0000

    step

--- src/navigation.js
+++ src/navigation.js
@@ -131,15 +131,15 @@ export const headerData = {
         },
         {
           text: 'Comboplan',
-          href: getPermalink('/comboplanpage'),
+          href: getPermalink('/combo/comboplanpage'),
         },
         {
           text: 'Comboplan eintragen',
-          href: getPermalink('/combotestpage'),
+          href: getPermalink('/combo/combotestpage'),
         },
         {
           text: 'Lieder',
-          href: getPermalink('/comboliederauswahlpage'),
+          href: getPermalink('/combo/comboliederauswahlpage'),
         },
       ],
       
@@ -150,54 +150,12 @@ export const headerData = {
 
 export const footerData = {
   links: [
-    {
-      title: 'Product',
-      links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
-      ],
-    },
-    {
-      title: 'Platform',
-      links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
-      ],
-    },
+    
   ],
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+    { text: 'Kontakt', href: getPermalink('/terms') },
+    { text: 'Impressum', href: getPermalink('/privacy') },
+    { text: 'DSGVO', href: getPermalink('/privacy') },
   ],
   socialLinks: [
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
@@ -207,7 +165,6 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
-    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
-    Made by <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+  Evangelische Messiaskapelle, Pfarrgemeinde A.B. Wien Alsergrund © 2023
   `,
 };
