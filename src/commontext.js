0721bea5095dbc097f8ee41ebc9e2b6092d57e48
Hans Wieser <hw@ds17.at>
2024-01-28 20:56:40 +0100

    Übersetzungen für englische Texte

---
+++ src/commontext.js
@@ -0,0 +1,24 @@
+export const commonText = {
+    text: {
+        page: "Seite",
+        post: "Post",
+        posts: "Posts"
+    },
+    blog: {
+        headLine: "Blog",
+        title: "Blog",
+        category: "Kategorie",
+        tag: "Tag"
+    },
+    category: {
+        title: "Kategorie"
+    },
+    tag: {
+        headLine: "Tag: ",
+        title: "Tag"
+    },
+    pagination: {
+        nextText: "Älter",
+        prevText: "Neuer"
+    }
+};
