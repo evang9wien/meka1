4399e2bf0325a88baad6534c81fc2d00c7104df1
Andreas Eidmann <eidmann@obm.at>
2024-02-26 22:20:08 +0100

    Bugfix: Blog Artikel (HTML, Headline)

--- src/commontext.js
+++ src/commontext.js
@@ -1,33 +1,33 @@
 export const commonText = {
-    text: {
-        page: "Seite",
-        post: "Post",
-        posts: "Posts"
-    },
-    blog: {
-        headLine: "Blog",
-        title: "Blog",
-        category: "Kategorie",
-        tag: "Tag"
-    },
-    category: {
-        title: "Kategorie"
-    },
-    tag: {
-        headLine: "Tag: ",
-        title: "Tag",
-        postsForTag: "Posts für Tag"
-    },
-    singlePost: {
-        minute: "Minute",
-        minutes: "Minuten",
-        redingTime: "Lesezeit"
-    },
-    pagination: {
-        nextText: "Älter",
-        prevText: "Neuer"
-    },
-    toBlogLink: {
-        backToBlog: "Zurück zum Blog"
-    }
+  text: {
+    page: 'Seite',
+    post: 'Post',
+    posts: 'Posts',
+  },
+  blog: {
+    headLine: 'Blog',
+    title: 'Blog',
+    category: 'Kategorie',
+    tag: 'Tag',
+  },
+  category: {
+    title: 'Kategorie',
+  },
+  tag: {
+    headLine: 'Alle Artikel zum Thema: ',
+    title: 'Tag',
+    postsForTag: 'Posts für Tag',
+  },
+  singlePost: {
+    minute: 'Minute',
+    minutes: 'Minuten',
+    redingTime: 'Lesezeit',
+  },
+  pagination: {
+    nextText: 'Älter',
+    prevText: 'Neuer',
+  },
+  toBlogLink: {
+    backToBlog: 'Zurück zum Blog',
+  },
 };
