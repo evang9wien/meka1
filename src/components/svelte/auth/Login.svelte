5d808f0ce6ded898a5bdda2ed074660bae240110
Andreas Eidmann <eidmann@obm.at>
2024-03-02 22:15:36 +0100

    Login: Anpassung des Fehlerhandlings

--- src/components/svelte/auth/Login.svelte
+++ src/components/svelte/auth/Login.svelte
@@ -4,11 +4,17 @@
   import { GradientButton } from 'flowbite-svelte';
   import { Label, Input } from 'flowbite-svelte';
   import { Card } from 'flowbite-svelte';
+  import { Alert } from 'flowbite-svelte';
+  import { InfoCircleSolid } from 'flowbite-svelte-icons';
+
+  export let loginReload = true;
 
   let name = '';
   let password = '';
 
   let loginSucess = false;
+
+  let loginError = false;
 
   const login = () => {
     console.log('Login');
@@ -18,10 +24,18 @@
         password: password,
       })
       .then((response) => {
+        console.log(response);
         const token = response.data.token;
         localStorage.setItem('jwt', token);
         loginSucess = true;
-        window.location.reload();
+        loginError = false;
+        if (loginReload) {
+          window.location.reload();
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        loginError = true;
       });
   };
 </script>
@@ -31,6 +45,13 @@
     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Login für Mitarbeiter</h5>
     <!-- <Title>Login</Title> -->
     {#if !loginSucess}
+      {#if loginError}
+        <Alert color="red">
+          <InfoCircleSolid slot="icon" class="w-4 h-4" />
+          <div class="font-medium">Login Fehler!</div>
+          Benutzername oder Passwort ist fehlerhaft.
+        </Alert>
+      {/if}
       <div class="mb-6">
         <Label for="default-input-n" class="block mb-2">Name</Label>
         <Input bind:value={name} id="default-input-n" placeholder="Name" />
@@ -41,7 +62,11 @@
       </div>
       <GradientButton class="mb-6" color="cyanToBlue" on:click={login}>Login</GradientButton>
     {:else}
-      Login erfolgreich
+      <Alert color="green">
+        <InfoCircleSolid slot="icon" class="w-4 h-4" />
+        <div class="font-medium">Login erfolgreich!</div>
+        Die Seiten im geschützen Bereich können nun geöffnet werden.
+      </Alert>
     {/if}
   </Card>
 </div>
