ea717009fabac045b8811978926fec8e7d4ee422
Andreas Eidmann <eidmann@obm.at>
2024-04-15 00:52:15 +0200

    firebase

--- src/components/svelte/ComboliederauswahlFB.svelte
+++ src/components/svelte/ComboliederauswahlFB.svelte
@@ -23,8 +23,21 @@
   import { getUrl } from './url/url.js';
 
   import { initializeApp } from 'firebase/app';
-  import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+  import { getStorage, ref as stref, getDownloadURL, connectStorageEmulator } from 'firebase/storage';
   import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+  import { getFirestore, doc, getDoc } from 'firebase/firestore';
+  import {
+    getDatabase,
+    ref as dbref,
+    onValue,
+    query,
+    orderByKey,
+    limitToLast,
+    limitToFirst,
+    startAt,
+  } from 'firebase/database';
+
+  import moment from 'moment';
 
   let selectedTermin;
   let lastSelectedTermin;
@@ -39,40 +52,38 @@
   let popupFireBaseLogin = false;
 
   let fireBase = true;
+
   const firebaseConfig = {
     apiKey: 'AIzaSyCCScuVEd_E1vQRPMkCuALcccPbly0JhPc',
     authDomain: 'evang9-combo-4cb8e.firebaseapp.com',
+    databaseURL: 'https://evang9-combo-4cb8e-default-rtdb.europe-west1.firebasedatabase.app',
     projectId: 'evang9-combo-4cb8e',
     storageBucket: 'evang9-combo-4cb8e.appspot.com',
     messagingSenderId: '110813316877',
-    appId: '1:110813316877:web:f5d52bbcd48b942b189244',
-    measurementId: 'G-0EW96D5XX2',
+    appId: '1:110813316877:web:2e6a8789144bb3e5189244',
+    measurementId: 'G-RC23VERNJ8',
   };
 
   let storage;
   let auth;
   onMount(async () => {
-    if (fireBase) {
-      console.log('FireBase');
-      const app = initializeApp(firebaseConfig);
-      auth = getAuth(app);
-      onAuthStateChanged(auth, (user) => {
-        if (user) {
-          // User is signed in, see docs for a list of available properties
-          // https://firebase.google.com/docs/reference/js/auth.user
-          const uid = user.uid;
-          console.log('User:  ', user);
-          // ...
-        } else {
-          // User is signed out
-          popupFireBaseLogin = true;
-          // ...
-        }
-      });
-      storage = getStorage(app);
-
-      const url = await getDownloadURL(ref(storage, 'lieder/mp3/above_all.mp3'));
-    }
+    console.log('FireBase');
+    const app = initializeApp(firebaseConfig);
+    auth = getAuth(app);
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const uid = user.uid;
+        console.log('User:  ', user);
+        // ...
+      } else {
+        // User is signed out
+        popupFireBaseLogin = true;
+        // ...
+      }
+    });
+    storage = getStorage(app);
 
     console.log('onMount');
     popupSpinnerModal = true;
@@ -82,16 +93,56 @@
       return;
     }
 
+    /// load firebase liederauswahl
+    console.log('Read Termine');
+    const dbFireStore = getFirestore(app);
+
+    const dbRealtime = getDatabase(app);
+    const now = moment().subtract(2, 'days').format('YYYY-MM-DD');
+    // console.log('Now: ', now.format('YYYY-MM-DD'));
+
+    const dbRef = query(dbref(dbRealtime, 'combo/termine'), orderByKey(), startAt(now), limitToFirst(1));
+    // console.log('Temine: ', dbRef);
+
+    onValue(dbRef, async (snapshot) => {
+      const termin = Object.values(snapshot.val())[0];
+      verantwortlich = termin.Verantwortlich;
+      selectedTermin = termin.Termin;
+      lastSelectedTermin = selectedTermin;
+      // updateStarCount(postElement, data);
+      console.log('Termin: ', termin);
+      // const liederAus = [];
+      liederauswahl = [];
+      termin.LiedAuswahl.forEach(async (l) => {
+        const liedRef = doc(dbFireStore, 'lieder', l.lied_liste_nummer);
+        const docSnap = await getDoc(liedRef);
+        if (docSnap.exists()) {
+          // console.log('Document data:', docSnap.data());
+          const o = { ...l, ...docSnap.data() };
+          liederauswahl.push(o);
+        } else {
+          // docSnap.data() will be undefined in this case
+          console.log('No such document!');
+        }
+      });
+      popupSpinnerModal = false;
+      // liederauswahl = liederAus;
+      console.log('Lieder: ', liederauswahl);
+      console.log('liederL: ', liederauswahl.length);
+      liederauswahl.forEach((e) => console.log('E: ', e));
+      // console.log('Lieder: ', liederAus);
+    });
+
     axios.get(getUrl() + '/root/wp-json/combo/v2/comboliederauswahl', getAuthHeader()).then((response) => {
       liederauswahl = JSON.parse(response.data);
       if (liederauswahl[0]) {
         const lied1 = liederauswahl[0];
         selectedTermin = lied1.Termin_Liedliste;
         lastSelectedTermin = selectedTermin;
-        verantwortlich = lied1.Verantwortlich;
+        // verantwortlich = lied1.Verantwortlich;
       }
       popupSpinnerModal = false;
-      // console.log(liederauswahl);
+      console.log('Lieder alt: ', liederauswahl);
     });
     axios.get(getUrl() + '/root/wp-json/combo/v1/combotermine?from_date=-30&to_date=30').then((response) => {
       termine = JSON.parse(response.data);
@@ -175,7 +226,7 @@
                   <TableBodyCell class="w-4">
                     <div class="flex flex-row">
                       {#if fireBase}
-                        {#await getDownloadURL(ref(storage, 'lieder/noten/' + lied.Dateiname + '.pdf'))}
+                        {#await getDownloadURL(stref(storage, 'lieder/noten/' + lied.Dateiname + '.pdf'))}
                           <p>loading</p>
                         {:then url}
                           <A href={url} target="_blank">
@@ -211,7 +262,7 @@
                     {#if lied.MP3 != '0'}
                       <div class="flex flex-row">
                         {#if fireBase}
-                          {#await getDownloadURL(ref(storage, 'lieder/mp3/' + lied.Dateiname + '.mp3'))}
+                          {#await getDownloadURL(stref(storage, 'lieder/mp3/' + lied.Dateiname + '.mp3'))}
                             <p>loading</p>
                           {:then url}
                             <audio controls src={url}></audio>
